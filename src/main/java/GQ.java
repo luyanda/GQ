aadfacee990cc7efe7bc4504a32889dc5924dd24
lm_mainaim <luyandambodla@gmail.com>
2019-03-01 03:11:01 +0200

    First Commit

--- src/main/java/GQ.java
+++ src/main/java/GQ.java
@@ -1,4 +1,13 @@
+import java.text.DecimalFormat;
+
 public class GQ {
+    public static void main(String []args){
+        int [][] arr = {{1,2}};
+        DecimalFormat decimalFormat = new DecimalFormat("#.######");
+        int x=50,y=3;
+        //double sum = Double.parseDouble();
+        System.out.println(arr.length);
+    }
     public Integer[] deleteElement(Integer [] arr, int index) {
 
         if(index<1 || index>arr.length)
@@ -18,5 +27,37 @@ public class GQ {
             return null;
         arr[index-1] = value;
         return arr;
+    }
+
+    public double QueryGraphs(Integer[] arr, int[][] edges, int [][] ops) {
+        if(ops.length==0)
+            return 0.0;
+        double A = 0.0, big=0;
+        int x = edges[0][0];
+        int y = edges[0][1];
+       // big = biggestWeight(big, edges, arr);
+        return A;
+    }
+
+    protected double biggestWeight(int [][] vertices, int x, int y, Integer[] arr) {
+        double big = 0;
+        for (int i=0;i<vertices.length;i++){
+            if(x == vertices[i][0] || x == vertices[i][1]){
+                if(arr[vertices[i][0]-1] != null)
+                    if(big < arr[vertices[i][0]-1])
+                    big = arr[vertices[i][0]-1];
+                if(arr[vertices[i][1]-1] != null)
+                    if(big < arr[vertices[i][1]-1])
+                    big = arr[vertices[i][1]-1];
+            }else if(y == vertices[i][0] || y == vertices[i][1]){
+                if(arr[vertices[i][0]-1] != null)
+                    if(big < arr[vertices[i][0]-1])
+                    big = arr[vertices[i][0]-1];
+                if(arr[vertices[i][1]-1] != null)
+                    if(big < arr[vertices[i][1]-1])
+                        big = arr[vertices[i][1]-1];
+            }
+        }
+        return big;
     }
 }
--- src/test/java/GQTest.java
+++ src/test/java/GQTest.java
@@ -14,7 +14,7 @@ public class GQTest {
         gq = new GQ();
     }
     @Test
-    @DisplayName("Should be equal")
+    @DisplayName("Should Delete Element")
     public void testDeleteElement(){
         Integer [] arr ={10,20,30,40,50};
         int [] res ={10,20,30,50};
@@ -23,17 +23,35 @@ public class GQTest {
         assertEquals(res[3], arr[3]);
     }
     @Test
-    @DisplayName("Shout replace values")
-    void testDeleteEquals(){
+    @DisplayName("Should replace values")
+    void testShouldReplaceElement(){
         Integer [] arr ={10,20,30,40,50};
-        int [] res ={10,20,60,40,50};
-        arr = gq.replaceElement(arr,3,60);
-        assertEquals(res[2], arr[2]);
+        int [] res ={60,20,60,40,50};
+        arr = gq.replaceElement(arr,1,60);
+        assertEquals(res[0], arr[0]);
     }
 
     @Test
     @DisplayName("Query weight of Larges")
     void testAvereageQuerie(){
+        Integer [] arr ={10,20,30,50};
+        int [][] edges ={{1,1},{1,2},{1,3}};
+        int x = 1,y = 1;
+        int [][] query = {{1,1},{1,2},{1,3}};
+        double res = 25.000000;
+        assertEquals(gq.QueryGraphs(arr,edges, query),res);
+    }
+
+    @Test
+    void testBigestWeight(){
+        Integer [] arr ={10};
+        int [][] edges ={{1,1},{1,2},{1,3}};
+        int [][] vertices = {{1,2},{2,3},{1,3},{2,4}};
+        int x = 1,y = 2;
+        int [][] opes = {{1,1},{1,2},{1,3}};
+        double res = 25.000000;
+        double big =0.0;
+        assertEquals(10.00,gq.biggestWeight(vertices,x,y,arr));
 
     }
 }
